e72ba99a73b13835d8b63d43b2033122288a5d2c
mother4 <mother4@mother4s-MacBook-Pro.local>
2024-10-31 13:54:32 +0100

    premier commit

---
+++ coursternaires.js
@@ -0,0 +1,12 @@
+const num = 101;
+
+const isMorethan100 = num > 100 ? true : false;
+console.log(isMorethan100);
+const num2 = 6;
+const result = num2 % 2 === 0 ? "pair" : "impair";
+console.log(result);
+
+const printA = () => {
+  console.log("A");
+};
+num2 === 5 && printA();
